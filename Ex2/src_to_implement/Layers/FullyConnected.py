official ex2 now in. Fixed a bunch of errors in the neural network implementation. In particular the softmax backprop

Ex2/src_to_implement/Layers/FullyConnected.py
@@ -23,7 +23,7 @@ class FullyConnected:
         output_size = error_tensor.shape[1]
         amount = self.last_input.shape[0]
         gradient = np.empty_like(self.weights)
-        gradient[:, -1] = error_tensor.sum(axis=0)/len(error_tensor)
+        gradient[:, -1] = error_tensor.sum(axis=0)
         differential = error_tensor.reshape(amount, output_size, 1) @ self.last_input.reshape(amount, 1, input_size)
         gradient[:, :-1] = differential.sum(axis=0)
         self.gradient_weights = gradient

Ex2/src_to_implement/Layers/SoftMax.py
@@ -5,7 +5,6 @@ class SoftMax:
         pass
     
     def forward(self, input_tensor):
-        self.last_actual_sum = np.exp(input_tensor).sum(axis=1).reshape(len(input_tensor), 1, 1)
         self.last_exp = np.exp(input_tensor - np.max(input_tensor, axis=1).reshape(len(input_tensor), 1))
         self.last_summed = self.last_exp.sum(axis=1).reshape(len(input_tensor), 1)
         return self.last_exp / self.last_summed
@@ -14,13 +13,10 @@ class SoftMax:
         amount = len(self.last_exp)
         classes = self.last_exp.shape[1]
 
-        exp = np.repeat(self.last_exp.reshape(amount, 1, classes), classes, axis=1)
-        transposed = exp.swapaxes(1, 2)
-        summed = self.last_summed.reshape(amount, 1, 1)
-        
-        diagonal = np.diag(np.ones(classes))
-        not_diagonal = 1.0*(diagonal == 0)
+        term1 = self.last_exp/(self.last_summed**2)
 
-        differential = (diagonal * exp/summed) + (not_diagonal * 1/self.last_actual_sum) - (exp * transposed)/(summed**2)
-        
+        diagonal = term1 * (self.last_summed - self.last_exp)
+        differential = -term1.reshape(amount, classes, 1) @ self.last_exp.reshape(amount, 1, classes)
+        differential.reshape(amount, classes**2)[:, ::classes+1] = diagonal
+
         return (error_tensor.reshape(amount, 1, classes) @ differential).reshape(amount, classes)

Ex2/src_to_implement/NeuralNetworkTests.py
@@ -5,6 +5,8 @@ import numpy as np
 import NeuralNetwork
 import matplotlib.pyplot as plt
 
+ID = 1  # identifier for dispatcher
+
 class TestFullyConnected(unittest.TestCase):
     def setUp(self):
         self.batch_size = 9
@@ -26,6 +28,7 @@ class TestFullyConnected(unittest.TestCase):
     def test_backward_size(self):
         layer = FullyConnected.FullyConnected(self.input_size, self.output_size)
         output_tensor = layer.forward(self.input_tensor)
+        # print(output_tensor.shape)
         error_tensor = layer.backward(output_tensor)
         self.assertEqual(error_tensor.shape[1], self.input_size)
         self.assertEqual(error_tensor.shape[0], self.batch_size)
@@ -135,7 +138,6 @@ class TestSoftMax(unittest.TestCase):
         loss_layer.forward(pred, self.label_tensor)
         error = loss_layer.backward(self.label_tensor)
         error = layer.backward(error)
-        print(error.sum())
         self.assertAlmostEqual(np.sum(error), 0)
 
     def test_regression_high_loss(self):
@@ -238,7 +240,7 @@ class TestCrossEntropyLoss(unittest.TestCase):
         layers = list()
         layers.append(Loss.CrossEntropyLoss())
         difference = Helpers.gradient_check(layers, input_tensor, self.label_tensor)
-        self.assertLessEqual(np.sum(difference), 1e-5)
+        self.assertLessEqual(np.sum(difference), 1e-4)
 
     def test_zero_loss(self):
         layer = Loss.CrossEntropyLoss()

Ex2/src_to_implement/Optimization/Loss.py
@@ -5,9 +5,10 @@ class CrossEntropyLoss:
         pass
 
     def forward(self, input_tensor, label_tensor):
+        input_tensor = input_tensor + (input_tensor == 0) * 13121e-145
         self.last_input = input_tensor
         return -(np.log(input_tensor) * label_tensor).sum()/len(input_tensor)
 
     def backward(self, label_tensor):
-        differential = -(label_tensor/self.last_input)
+        differential = -(label_tensor/self.last_input)/len(label_tensor)
         return differential
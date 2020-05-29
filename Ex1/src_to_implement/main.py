9a70a10d5be1883b81bbc6e5ce30fbd7724cfc56
Jonas Beyer <jonas.beyer@fau.de>
2020-05-29 14:52:39 +0200

    showing pictures in main

--- Ex1/src_to_implement/main.py
+++ Ex1/src_to_implement/main.py
@@ -1,12 +1,17 @@
 from pattern import Circle, Checker
+from generator import ImageGenerator
 import matplotlib.pyplot as plt
 
 c = Circle(100, 10, (50, 50))
 c.draw()
-plt.imshow(c.output)
+plt.imshow(c.output, cmap='gray')
 plt.show()
 
 c = Checker(100, 5)
 c.draw()
-plt.imshow(c.output)
+plt.imshow(c.output, cmap='gray')
 plt.show()
+
+gen = ImageGenerator('exercise_data', 'Labels.json', 10, (64, 64, 3), True, True, True)
+
+gen.show()
